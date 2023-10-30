3d15c53f1f840f6bd4a51525b0dbb49f7287c076
Brady Lin <linbrady16877@gmail.com>
2023-10-30 00:58:18 -0700

    CHanged player to UFO, added rocket and box

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -7,65 +7,105 @@ class Play extends Phaser.Scene{
         // Load images/tile sprites
         this.load.path = './assets/'
         this.load.image("playBackground", "playBackground.png");
-        // this.load.spritesheet("playerRun", "PlayerRun.png", {
-        //     frameWidth: 32,
-        //     frameHeight: 32
-        // })
-        this.load.atlas("playerRun", "Sprite-0001.png", "Sprite-0001.json")
+        this.load.atlas("playerRun", "UFO.png", "UFO.json")
+        this.load.image("box", "Box Sprite.png")
+        this.load.atlas("rocket", "Rocket.png")
     }
 
     create(){
         // Add background image
         this.bg = this.add.tileSprite(0, 0, game.config.width, game.config.height, "playBackground").setOrigin(0, 0).setScale(10.5);
 
-        // Add player image
-        this.player = this.add.sprite(this.game.config.width / 2 - 500, this.game.config.height / 2 + 220, "playerRun").setScale(10);
+        // Add player
+        this.player = this.physics.add.sprite(game.config.width / 2 - 500, game.config.height / 2 + 220, "playerRun", "UFO 0.ase").setScale(10);
+        this.player.body.setSize(12, 9, true);
+        this.player.body.setCollideWorldBounds(true);
 
-        // this.textures.addSpriteSheet("playerRun", {
-        //     atlas: "playerRun",
-        //     frameMax: 4,
-        //     frameMin: 0,
-        //     startFrame: 0,
-        //     endFrame: 0,
-        //     margin: 0,
-        //     spacing: 0
-        // })
-
-        this.textures.addSpriteSheetFromAtlas("playerRun", {
-            atlas: "playerRun",
-            frameMax: 3,
-            frameMin: 0,
-            startFrame: 0,
-            endFrame: 0,
-            margin: 0,
-            spacing: 0
-        })
-
-        // Add player animation
+        this.textures.addSpriteSheetFromAtlas("UFO 0.ase", {frameHeight: 32, frameWidth: 16, atlas: "playerRun", frame: "UFO 0.ase"})
+        
         this.anims.create({
             key: "run",
-            frameRate: 2,
+            frameRate: 4,
             repeat: -1,
-            frames: this.anims.generateFrameNumbers("playerRun", {
-                prefix: "Sprite-0001 ",
+            frames: this.anims.generateFrameNames("playerRun", {
+                prefix: "UFO ",
                 start: 0,
                 end: 3,
-                suffix: ".LibreSprite"
+                suffix: ".ase"
             })
-
         })
         
+
         this.player.anims.play("run");
 
+        // Add Rocket
+        this.rocket = this.physics.add.sprite(game.config.width / 2 + 500, game.config.height / 2 + 220, "rocket").setScale(10);
+        this.textures.addSpriteSheetFromAtlas("Rocket 0.ase", {frameHeight: 32, frameWidth: 16, atlas: "rocket", frame: "Rocket 0.ase"})
+        this.rocket.body.setImmovable(true);
+        // this.rocket.setX(Phaser.Math.Between(game.config.length, game.config.width - this.rocket.width / 2))
 
-    
+        this.anims.create({
+            key: "rocket",
+            frameRate: 4,
+            repeat: -1,
+            frames: this.anims.generateFrameNames("rocket", {
+                prefix: "Rocket ",
+                start: 0,
+                end: 3,
+                suffix: ".ase"
+            })
+
+        })                    
+
+        this.rocket.anims.play("rocket");
+        this.rocket.setVelocityX(-200);
+
+
+        // Add box
+        this.box = this.physics.add.sprite(game.config.width / 2 + 500, game.config.height / 2 + 220, "box").setScale(10);
+        this.box.body.setImmovable(true);
+        this.box.body.setSize(6, 6, true);
+
+
+
+        // Add collision
+
+            // box colliders
+        this.physics.add.collider(this.player, this.box, () => {
+            this.scene.start("playScene");
+        })
+
+
    
     }
 
     update(){
+        let playerVector = new Phaser.Math.Vector2(0,0)
+
         // Scroll the background
         this.bg.tilePositionX += 0.5;
 
+        // Box moving towards player
+        this.box.x -= 1;
+
+        // Rocket moving towards player
+        this.rocket.x -= 2;
+
+        if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W).isDown){
+            // Player moves up
+            playerVector.y = -200
+        }
+
+        else if (this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S).isDown){
+            // Player moves down
+            playerVector.y = 200
+        }
+        else {
+            playerVector.y = 0
+        }
+
+        this.player.body.setVelocity(playerVector.x, playerVector.y);
 
     }
+
 }
